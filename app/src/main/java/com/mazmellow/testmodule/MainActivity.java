ec946173b20924c18da463f19afcfccc154a646b
Maz <watcharat.s@gmail.com>
2016-09-12 17:05:45 +0700

    add sdk activity.

--- app/src/main/java/com/mazmellow/testmodule/MainActivity.java
+++ app/src/main/java/com/mazmellow/testmodule/MainActivity.java
@@ -1,7 +1,12 @@
 package com.mazmellow.testmodule;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.mazmellow.testmodule.sdk.SdkActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSdk = (Button)findViewById(R.id.btnSdk);
+        btnSdk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = SdkActivity.buildIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
     }
 }
